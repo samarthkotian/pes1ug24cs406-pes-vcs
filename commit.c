5ef65ebdbc53834452183f3a21220a82db30863b
samarth kotian <samarth9080@gmail.com>
2026-05-05 16:32:46 +0530

    phase4

--- commit.c
+++ commit.c
@@ -194,8 +194,33 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    Commit c;
+    memset(&c, 0, sizeof(c));
+    if (tree_from_index(&c.tree) != 0)
+      return -1;
+    if (head_read(&c.parent) == 0)
+      c.has_parent = 1;
+    else
+      c.has_parent = 0;
+
+    snprintf(c.author, sizeof(c.author), "%s", pes_author());
+    c.timestamp = (uint64_t)time(NULL);
+    snprintf(c.message, sizeof(c.message), "%s", message);
+    void *data;
+    size_t len;
+
+    if (commit_serialize(&c, &data, &len) != 0)
+      return -1;
+
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0) {
+      free(data);
+      return -1;
 }
+
+   free(data);
+   if (head_update(commit_id_out) != 0)
+    return -1;
+
+    return 0;
+ }
+
